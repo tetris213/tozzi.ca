d79f2189a703501a85c31beb42e6f90ede9adc5c
tetris213 <tatzi@a2plcpnl0597.prod.iad2.secureserver.net>
2017-04-23 15:30:11 -0700

    live site

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>test page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Test page</div>
+            </div>
+        </div>
+    </body>
+</html>
